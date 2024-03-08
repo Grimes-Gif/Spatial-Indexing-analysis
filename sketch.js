1183063a0a7b0df86276d0ba3f0942d1e43eb661
Micah Grimes <mgrimes33@gatech.edu>
2024-03-08 12:53:41 -0500

    Fixed problem where depending on how selected area was draw points would not be counted as in the region

--- sketch.js
+++ sketch.js
@@ -5,8 +5,6 @@ var region_list = []
 var x, y
 const [REGION_MIN_X, REGION_MIN_Y] = [10, 10]
 
-var timer = 0
-
 function setup() {
   createCanvas(windowWidth, windowHeight)
   qt = new QTree(new AABB(0,0,windowWidth, windowHeight), 5)
@@ -17,7 +15,6 @@ function draw() {
 
   if (mouseIsPressed) {
     showSelectedRegion()
-    timer++
   }
 
   point_list.forEach((point) => {
@@ -40,28 +37,28 @@ function mousePressed() {
 }
 
 function mouseDragged() {
-  if (x - mouseX > 0) { //mouse is going left
-    inverted = true
-  } else {
-    inverted = false
-  }
+
 }
 
 function showSelectedRegion() {
   noFill()
-  stroke('black')
+  stroke('gray')
   rect(x, y, mouseX-x, mouseY-y)
 }
 
 function mouseReleased() {
-  if (timer > 15 && Math.abs(mouseX-x) > REGION_MIN_X && Math.abs(mouseY-y) > REGION_MIN_Y) {
-    let selected_points = qt.queryRegion(region_list[region_list.push(new AABB(x, y, mouseX-x, mouseY-y))-1])
+  let width = Math.abs(mouseX-x)
+  let height = Math.abs(mouseY-y)
+  if (width > REGION_MIN_X && height > REGION_MIN_Y) {
+    let cx = (mouseX + x) / 2
+    let cy = (mouseY + y) / 2
+    let selected_points = qt.queryRegion(region_list[region_list.push(new AABB(cx - width/2,cy - height/2, width, height))-1])
     selected_points.forEach((point) => {
       point.isSelected = true
     })
-    console.log('pushed a region, ', region_list[-1], ' that contains: ', selected_points.length, ' points')
-  } else if (timer <= 15) {
+    
+    console.log('pushed a region, ', region_list[region_list.length-1], ' that contains: ', selected_points.length, ' points')
+  } else {
     point_list.push(qt.insert(new point(mouseX, mouseY)))
   }
-  timer = 0
 }
